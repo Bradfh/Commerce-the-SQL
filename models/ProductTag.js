203ab3211fcaab5ff822ae3834b23481b274d0d5
Bradfh <128874401+Bradfh@users.noreply.github.com>
2023-06-22 15:32:16 -0500

    Got the colums set up and the database seeded

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,8 +6,30 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    product_tag_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
   },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'Product_id',
+        onDelete: 'CASCADE',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'Tag_id',
+        onDelete: 'CASCADE',
+      },
+    },
+  },
+
   {
     sequelize,
     timestamps: false,
